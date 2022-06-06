a36adaaf90538a95b5fe8239d138e49d16b8ac44
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2022-06-07 00:21:11 +0300

    intersection2arr v2

--- coding/leetcode/go/intersection2arr/main.go
+++ coding/leetcode/go/intersection2arr/main.go
@@ -1,5 +1,9 @@
 package intersection2arr
 
+import (
+	"sort"
+)
+
 func intersect(nums1 []int, nums2 []int) []int {
 
 	if len(nums1) < len(nums2) {
@@ -18,6 +22,36 @@ func intersect(nums1 []int, nums2 []int) []int {
 		if ok && v != 0 {
 			result = append(result, n)
 			tempH[n]--
+		}
+	}
+	return result
+}
+
+func intersect2(nums1 []int, nums2 []int) []int {
+	// Time: O(mlogm + nlogn)
+	// Space: O(1)
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+
+	var result []int
+
+	var i, j int
+
+	for x := 0; x < len(nums1)+len(nums2); x++ {
+		if j >= len(nums2) {
+			break
+		}
+		if i >= len(nums1) {
+			break
+		}
+		if nums2[j] == nums1[i] {
+			result = append(result, nums2[j])
+			j++
+			i++
+		} else if nums2[j] > nums1[i] {
+			i++
+		} else {
+			j++
 		}
 	}
 	return result
--- coding/leetcode/go/intersection2arr/main_test.go
+++ coding/leetcode/go/intersection2arr/main_test.go
@@ -6,24 +6,29 @@ import (
 	"testing"
 )
 
-func TestIntersect(t *testing.T) {
-	test := []struct {
-		nums1 []int
-		nums2 []int
-		want  []int
-	}{
-		{
-			[]int{1, 2, 2, 1},
-			[]int{2, 2},
-			[]int{2, 2},
-		},
-		{
-			[]int{4, 9, 5},
-			[]int{9, 4, 9, 8, 4},
-			[]int{4, 9},
-		},
-	}
+var test = []struct {
+	nums1 []int
+	nums2 []int
+	want  []int
+}{
+	{
+		[]int{1, 2, 2, 1},
+		[]int{2, 2},
+		[]int{2, 2},
+	},
+	{
+		[]int{4, 9, 5},
+		[]int{9, 4, 9, 8, 4},
+		[]int{4, 9},
+	},
+	{
+		[]int{0, 5, 8, 7, 2, 9, 7, 5},
+		[]int{1, 4, 8, 9},
+		[]int{8, 9},
+	},
+}
 
+func TestIntersect(t *testing.T) {
 	for _, tt := range test {
 		result := intersect(tt.nums1, tt.nums2)
 		sort.Ints(result)
@@ -32,4 +37,35 @@ func TestIntersect(t *testing.T) {
 		}
 	}
 
+	for _, tt := range test {
+		result := intersect2(tt.nums1, tt.nums2)
+		sort.Ints(result)
+		if !reflect.DeepEqual(result, tt.want) {
+			t.Errorf("Got: %v, want: %v", result, tt.want)
+		}
+	}
+}
+
+func BenchmarkIntersect(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tt := range test {
+			result := intersect(tt.nums1, tt.nums2)
+			sort.Ints(result)
+			if !reflect.DeepEqual(result, tt.want) {
+				b.Errorf("Got: %v, want: %v", result, tt.want)
+			}
+		}
+	}
+}
+
+func BenchmarkIntersect2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tt := range test {
+			result := intersect2(tt.nums1, tt.nums2)
+			sort.Ints(result)
+			if !reflect.DeepEqual(result, tt.want) {
+				b.Errorf("Got: %v, want: %v", result, tt.want)
+			}
+		}
+	}
 }
