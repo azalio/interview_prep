517f4b18bbf68aa1b329fd984addcfd832bbd068
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2022-07-14 00:25:32 +0300

    isMorph ver2

--- coding/leetcode/go/isomorficStrings/main.go
+++ coding/leetcode/go/isomorficStrings/main.go
@@ -1,20 +1,14 @@
 package isomorficStrings
 
-import "fmt"
-
 func isIsomorphic(s string, t string) bool {
 
 	tmpH := make(map[uint8]uint8)
-
-	//"paper",
-	//"title",
+	tmpH2 := make(map[uint8]uint8)
 
 	for i, v := range t {
-		fmt.Printf("s[i]: %d, t[i]: %d\n", s[i], t[i])
 		val, ok := tmpH[uint8(v)]
 
 		if !ok {
-			fmt.Printf("val: %d\n", val)
 			tmpH[uint8(v)] = s[i]
 			continue
 		}
@@ -24,14 +18,10 @@ func isIsomorphic(s string, t string) bool {
 		}
 	}
 
-	tmpH2 := make(map[uint8]uint8)
-
 	for i, v := range s {
-		fmt.Printf("s[i]: %d, t[i]: %d\n", s[i], t[i])
 		val, ok := tmpH2[uint8(v)]
 
 		if !ok {
-			fmt.Printf("val: %d\n", val)
 			tmpH2[uint8(v)] = t[i]
 			continue
 		}
@@ -40,28 +30,35 @@ func isIsomorphic(s string, t string) bool {
 			return false
 		}
 	}
+	return true
+}
 
-	//tmpH := make(map[uint8]uint8)
-	//tmp2H := make(map[uint8]uint8)
-	//
-	//  "badc",
-	//	"coba",
+func isIsomorphic2(s string, t string) bool {
 
-	//for i, v := range s {
-	//	fmt.Printf("s[i]: %d, t[i]: %d\n", s[i], t[i])
-	//	val, ok := tmpH[uint8(v)]
-	//	val2, ok2 := tmp2H[uint8(v)]
-	//
-	//	if !ok && !ok2 {
-	//		tmpH[uint8(v)] = t[i]
-	//		tmp2H[t[i]] = uint8(v)
-	//
-	//	} else if val != t[i] || val2 != s[i] {
-	//		return false
-	//	}
-	//	fmt.Printf("%v\n", tmpH)
-	//	fmt.Printf("%v\n", tmp2H)
-	//
-	//}
+	tmpH := make(map[uint8]uint8)
+	tmpH2 := make(map[uint8]uint8)
+
+	for i := range s {
+
+		val, ok := tmpH[s[i]]
+		val2, ok2 := tmpH2[t[i]]
+
+		if ok {
+			if val != t[i] {
+				return false
+			}
+		}
+
+		if ok2 {
+			if val2 != s[i] {
+				return false
+			}
+		}
+
+		tmpH[s[i]] = t[i]
+		tmpH2[t[i]] = s[i]
+
+	}
+
 	return true
 }
--- coding/leetcode/go/isomorficStrings/main_test.go
+++ coding/leetcode/go/isomorficStrings/main_test.go
@@ -13,9 +13,9 @@ var test = []struct {
 		true,
 	},
 	{
-		"foo",
-		"bar",
-		false,
+		"paper",
+		"title",
+		true,
 	},
 	{
 		"paper",
@@ -27,6 +27,17 @@ var test = []struct {
 		"baba",
 		false,
 	},
+	{
+		"bad",
+		"bab",
+		false,
+	},
+
+	{
+		"foo",
+		"bar",
+		false,
+	},
 }
 
 func TestIsIsomorphic(t *testing.T) {
@@ -34,6 +45,37 @@ func TestIsIsomorphic(t *testing.T) {
 		result := isIsomorphic(tt.s, tt.t)
 		if result != tt.want {
 			t.Errorf("Got: %t, want: %t\n", result, tt.want)
+		}
+	}
+}
+
+func TestIsIsomorphic2(t *testing.T) {
+	for _, tt := range test {
+		result := isIsomorphic2(tt.s, tt.t)
+		if result != tt.want {
+			t.Errorf("Got: %t, want: %t\n", result, tt.want)
+		}
+	}
+}
+
+func BenchmarkTestIsIsomorphic(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tt := range test {
+			result := isIsomorphic(tt.s, tt.t)
+			if result != tt.want {
+				b.Errorf("Got: %t, want: %t\n", result, tt.want)
+			}
+		}
+	}
+}
+
+func BenchmarkTestIsIsomorphic2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tt := range test {
+			result := isIsomorphic2(tt.s, tt.t)
+			if result != tt.want {
+				b.Errorf("Got: %t, want: %t\n", result, tt.want)
+			}
 		}
 	}
 }
