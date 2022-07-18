39523d10a8358bd071379534a57a9f0f3071cab7
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2022-07-18 20:40:37 +0300

    isPalindrome ver2

--- coding/leetcode/go/isPalindrome/main.go
+++ coding/leetcode/go/isPalindrome/main.go
@@ -1,1 +1,50 @@
 package isPalindrome
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+
+	if x < 0 {
+		return false
+	}
+
+	xS := fmt.Sprint(x)
+
+	i, y := 0, len(xS)-1
+
+	for i <= y {
+		if xS[i] != xS[y] {
+			return false
+		}
+		i++
+		y--
+	}
+
+	return true
+}
+
+func isPalindrome2(x int) bool {
+
+	if x < 0 {
+		return false
+	}
+
+	var revNum int
+
+	tmpD := x
+
+	for {
+		lastD := tmpD % 10
+		revNum = revNum*10 + lastD
+		tmpD = tmpD / 10
+		if tmpD == 0 {
+			break
+		}
+	}
+
+	if revNum == x {
+		return true
+	}
+
+	return false
+}
--- coding/leetcode/go/isPalindrome/main_test.go
+++ coding/leetcode/go/isPalindrome/main_test.go
@@ -1,1 +1,75 @@
 package isPalindrome
+
+import "testing"
+
+var test = []struct {
+	x    int
+	want bool
+}{
+	{
+		121,
+		true,
+	},
+	{
+		-121,
+		false,
+	},
+	{
+		10,
+		false,
+	},
+	{
+		1010101,
+		true,
+	},
+	{
+		1111111111011111111,
+		false,
+	},
+	{
+		1111111111111111111,
+		true,
+	},
+}
+
+func TestIsPalindrome(t *testing.T) {
+
+	for _, tt := range test {
+		result := isPalindrome(tt.x)
+		if result != tt.want {
+			t.Errorf("x: %d, want: %t, got: %t\n", tt.x, tt.want, result)
+		}
+	}
+}
+
+func TestIsPalindrome2(t *testing.T) {
+
+	for _, tt := range test {
+		result := isPalindrome2(tt.x)
+		if result != tt.want {
+			t.Errorf("x: %d, want: %t, got: %t\n", tt.x, tt.want, result)
+		}
+	}
+}
+
+func BenchmarkTestIsPalindrome(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tt := range test {
+			result := isPalindrome(tt.x)
+			if result != tt.want {
+				b.Errorf("x: %d, want: %t, got: %t\n", tt.x, tt.want, result)
+			}
+		}
+	}
+}
+
+func BenchmarkTestIsPalindrome2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tt := range test {
+			result := isPalindrome2(tt.x)
+			if result != tt.want {
+				b.Errorf("x: %d, want: %t, got: %t\n", tt.x, tt.want, result)
+			}
+		}
+	}
+}
