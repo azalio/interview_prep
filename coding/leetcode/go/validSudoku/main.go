4095c2e3c127ca933366f92c779cf92e0f959b7b
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2022-08-01 01:15:04 +0300

    ValidSUdoku

--- coding/leetcode/go/validSudoku/main.go
+++ coding/leetcode/go/validSudoku/main.go
@@ -1,5 +1,78 @@
 package validSudoku
 
-func isValidSudoku(board [][]byte) bool {
+import (
+	"errors"
+)
 
+func getNum(s byte) (int, error) {
+	if s == 46 {
+		return -1, errors.New("not digit")
+	} else {
+		return int(s), nil
+	}
+}
+
+func isValidSudoku(board [][]byte) bool {
+	sudoku := make(map[int]bool)
+
+	for _, h := range board {
+		for _, z := range h {
+			n, err := getNum(z)
+			if err != nil {
+				continue
+			} else {
+				_, ok := sudoku[n]
+				if ok {
+					return false
+				}
+				sudoku[n] = true
+			}
+		}
+		sudoku = make(map[int]bool)
+	}
+
+	for j := 0; j < 9; j++ {
+		for i := 0; i < 9; i++ {
+			n, err := getNum(board[i][j])
+			if err != nil {
+				continue
+			} else {
+				_, ok := sudoku[n]
+				if ok {
+					return false
+				}
+				sudoku[n] = true
+			}
+		}
+
+		sudoku = make(map[int]bool)
+	}
+
+	for i := 0; i < 9; i = i + 3 {
+		nineToTheree := board[i : i+3]
+
+		for j := 0; j < 9; j = j + 3 {
+			for _, h := range nineToTheree {
+				txt := h[j : j+3]
+				for _, n := range txt {
+					n, err := getNum(n)
+					if err != nil {
+						continue
+					} else {
+						_, ok := sudoku[n]
+						if ok {
+							return false
+						}
+						sudoku[n] = true
+					}
+				}
+			}
+
+			sudoku = make(map[int]bool)
+
+		}
+
+	}
+
+	return true
 }
