456825adf6405e30f03017ba52702a876877e41a
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2022-10-01 22:24:15 +0300

    from tbank

--- coding/tbank/array_one/main.go
+++ coding/tbank/array_one/main.go
@@ -1,7 +1,53 @@
 package array_one
 
+import (
+	"sort"
+)
+
 //дан массив чисел, [4,3,0,1] несортированый, от 0 до N надо найти число которого не хватает,
 //ну и там по скорости или по времени лучшее решение предложить
 //ну и пограничные случаи типа 0 и N учесть
 
+func findNumber(s []int) int { // O(n) - cpu, O(n) - mem
+	sM := make(map[int]bool)
+	for _, v := range s {
+		sM[v] = true
+	}
 
+	for i := 0; i < len(s); i++ {
+		_, ok := sM[i]
+		if !ok {
+			return i
+		}
+	}
+	return 0
+}
+
+func findNumber2(s []int) int { // O(nlogn) - cpu, O(log n) - mem
+	sort.Ints(s)
+	ii := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] != ii {
+			return i
+		}
+		ii++
+	}
+	return 0
+}
+
+func findNumber3(s []int) int { // O(N) - cpu, O(1) - mem
+
+	if len(s) == 1 {
+		return 0
+	}
+
+	sumOfWholeSlice := (len(s)) * (len(s) + 1) / 2
+	//fmt.Println(sumOfWholeSlice)
+	var sumOfRealSlice int
+	for _, v := range s {
+		sumOfRealSlice += v
+	}
+
+	//fmt.Println(sumOfRealSlice)
+	return sumOfWholeSlice - sumOfRealSlice
+}
--- coding/tbank/array_one/main_test.go
+++ coding/tbank/array_one/main_test.go
@@ -1,1 +1,117 @@
 package array_one
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+var resultB int
+
+var test = []struct {
+	s    []int
+	want int
+}{
+	{
+		[]int{0, 2, 3, 4},
+		1,
+	},
+	{
+		[]int{0},
+		0,
+	},
+	//{
+	//	[]int{1, 2, 3, 4, 0},
+	//	0,
+	//},
+	{
+		[]int{3, 4, 6, 1, 2, 0},
+		5,
+	},
+	//{
+	//	[]int{6, 3, 1, 2, 0},
+	//	4,
+	//},
+}
+
+func TestFindNumber(t *testing.T) {
+	for _, tt := range test {
+		result := findNumber(tt.s)
+		if result != tt.want {
+			t.Errorf("\nGot %d, want: %d\n", result, tt.want)
+		}
+	}
+}
+
+func TestFindNumber2(t *testing.T) {
+	for _, tt := range test {
+		result := findNumber2(tt.s)
+		if result != tt.want {
+			t.Errorf("\nGot %d, want: %d\n", result, tt.want)
+		}
+	}
+}
+
+func TestFindNumber3(t *testing.T) {
+	for _, tt := range test {
+		result := findNumber3(tt.s)
+		if result != tt.want {
+			t.Errorf("\nGot %d, want: %d\n", result, tt.want)
+		}
+	}
+}
+
+func remove(s []int, i int) []int {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+func genBigSlice() []int {
+	var result []int
+	for i := 0; i < 10000000; i++ {
+		result = append(result, i)
+	}
+
+	result = remove(result, 31337)
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(result), func(i, j int) { result[i], result[j] = result[j], result[i] })
+	return result
+}
+
+func BenchmarkFindNumber(b *testing.B) {
+	var r int
+	for n := 0; n < b.N; n++ {
+		for _, tt := range test {
+			r = findNumber(tt.s)
+		}
+
+		r = findNumber(genBigSlice())
+	}
+	resultB = r
+}
+
+func BenchmarkFindNumber2(b *testing.B) {
+	var r int
+	for n := 0; n < b.N; n++ {
+		for _, tt := range test {
+			r = findNumber2(tt.s)
+		}
+		r = findNumber(genBigSlice())
+	}
+	resultB = r
+}
+
+func BenchmarkFindNumber3(b *testing.B) {
+	var r int
+	for n := 0; n < b.N; n++ {
+		for _, tt := range test {
+			r = findNumber3(tt.s)
+		}
+		r = findNumber(genBigSlice())
+	}
+	resultB = r
+}
+
+//go test -bench=. -benchmem
+//BenchmarkFindNumber-8                946           1214843 ns/op          813886 B/op        340 allocs/op
+//BenchmarkFindNumber2-8               984           1218791 ns/op          814004 B/op        345 allocs/op
