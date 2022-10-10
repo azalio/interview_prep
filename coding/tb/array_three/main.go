b4db9be341fc8607d62e75d2847535843582f31f
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2022-10-11 01:48:49 +0300

    commonElements in 3 arrays

--- coding/tb/array_three/main.go
+++ coding/tb/array_three/main.go
@@ -1,3 +1,9 @@
 package array_three
 
 //Первой оказалась задача на поиск одинаковых элементов в 3 отсортированных массивах с использованием O(1) памяти.
+// https://leetcode.com/problems/intersection-of-three-sorted-arrays/
+
+func findCommon(a, b, c []int) []int {
+	var result []int
+	return result
+}
--- coding/tb/array_three/main_test.go
+++ coding/tb/array_three/main_test.go
@@ -1,1 +1,35 @@
 package array_three
+
+import (
+	"reflect"
+	"testing"
+)
+
+var test = []struct {
+	a    []int
+	b    []int
+	c    []int
+	want []int
+}{
+	{
+		[]int{1, 5, 10, 20, 40, 80},
+		[]int{6, 7, 20, 80, 100},
+		[]int{3, 4, 15, 20, 30, 70, 80, 120},
+		[]int{20, 80},
+	},
+	{
+		[]int{1, 5, 5},
+		[]int{3, 4, 5, 5, 10},
+		[]int{5, 5, 10, 20},
+		[]int{5, 5},
+	},
+}
+
+func TestFindCommon(t *testing.T) {
+	for _, tt := range test {
+		result := findCommon(tt.a, tt.b, tt.c)
+		if !reflect.DeepEqual(result, tt.want) {
+			t.Errorf("\nWant: %v, got: %v\na: %v\nb: %v\nc: %v\n", tt.want, result, tt.a, tt.b, tt.c)
+		}
+	}
+}
