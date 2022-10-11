e51982c01d7a6a03370c35cf32843a116a82fed8
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2022-10-12 02:02:46 +0300

    single number

--- coding/tb/follow_up_marina/main.go
+++ coding/tb/follow_up_marina/main.go
@@ -3,3 +3,22 @@ package follow_up_marina
 // кстати, советую еще решить похожее - есть массив,
 // в котором все числа кроме одного - по 2 раза.
 // найти единственное число
+// https://leetcode.com/problems/single-number/
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+func singleNumber(nums []int) int {
+	var addA = make(map[int]int, len(nums)/2+1)
+
+	for _, v := range nums {
+		addA[v]++
+	}
+
+	for k, v := range addA {
+		if v == 1 {
+			return k
+		}
+	}
+
+	return 0
+}
--- coding/tb/follow_up_marina/main_test.go
+++ coding/tb/follow_up_marina/main_test.go
@@ -1,1 +1,30 @@
 package follow_up_marina
+
+import "testing"
+
+var test = []struct {
+	nums []int
+	want int
+}{
+	{
+		[]int{2, 2, 1},
+		1,
+	},
+	{
+		[]int{4, 1, 2, 1, 2},
+		4,
+	},
+	{
+		[]int{1},
+		1,
+	},
+}
+
+func TestSingleNumber(t *testing.T) {
+	for _, tt := range test {
+		result := singleNumber(tt.nums)
+		if result != tt.want {
+			t.Errorf("Got: %d, Want: %d\n", result, tt.want)
+		}
+	}
+}
