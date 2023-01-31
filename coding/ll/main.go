38089fa7c4ee74a0a095e6bb286cdf280b397352
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2023-02-01 01:21:50 +0300

    Implementing LL in go

---
+++ coding/ll/main.go
@@ -0,0 +1,142 @@
+package ll
+
+import (
+	"fmt"
+)
+
+// Node represents a node of linked list
+type Node struct {
+	value int
+	next  *Node
+}
+
+// LinkedList represents a linked list
+type LinkedList struct {
+	head *Node
+	len  int
+}
+
+func (l *LinkedList) Insert(val int) {
+	n := Node{}
+	n.value = val
+
+	if l.len == 0 {
+		l.head = &n
+		l.len++
+		return
+	}
+
+	current := l.head
+	for i := 0; i < l.len; i++ {
+
+		if current.next == nil {
+			current.next = &n
+			l.len++
+			break
+		} else {
+			current = current.next
+		}
+
+	}
+}
+
+func (l LinkedList) GetNode(pos int) *Node {
+
+	if pos == 0 {
+		return l.head
+	}
+
+	//fmt.Printf("pos is: %d\n", pos)
+	//fmt.Printf("l.len: %d\n", l.len)
+	//fmt.Printf("l.head: %+v\n", l.head)
+
+	for i := 0; ; i++ {
+		//fmt.Printf("i: %d\n", i)
+		if i == pos {
+			return l.head
+		} else {
+			l.head = l.head.next
+		}
+	}
+}
+
+func (l *LinkedList) InsertAt(pos int, value int) {
+	if l.len < pos {
+		return
+	}
+
+	n := Node{}
+	n.value = value
+
+	if pos == 0 {
+		l.head, n.next = &n, l.head
+		l.len++
+		return
+	}
+
+	if pos > l.len {
+		return
+	}
+
+	node := l.GetNode(pos)
+	//fmt.Printf("node value: %d\n", node.value)
+
+	prevNode := l.GetNode(pos - 1)
+	//fmt.Printf("prev node value: %d\n", prevNode.value)
+
+	prevNode.next = &n
+	n.next = node
+	l.len++
+
+}
+
+func (l LinkedList) PrintL() {
+
+	if l.len == 0 {
+		return
+	}
+	current := l.head
+
+	for i := 0; i < l.len; i++ {
+		fmt.Println(current.value)
+		current = current.next
+	}
+}
+
+func main() {
+	//d := Node{4, nil}
+	//c := Node{3, &d}
+	//b := Node{2, &c}
+	//a := Node{1, &b}
+	//
+	//current := a
+	//for current.next != nil {
+	//	fmt.Println(current.value)
+	//	current = *current.next
+	//}
+
+	//fmt.Println("Use LinkList structure")
+
+	ll := LinkedList{}
+	ll.Insert(1)
+	ll.Insert(2)
+	ll.Insert(3)
+	ll.Insert(4)
+
+	//for i := 0; i < ll.len; i++ {
+	//	fmt.Println(ll.head.value)
+	//	ll.head = ll.head.next
+	//}
+
+	ll.PrintL()
+
+	ll.InsertAt(1, 11)
+
+	ll.PrintL()
+
+	fmt.Printf("Inset 0 at 0 pos\n")
+	ll.InsertAt(0, 0)
+
+	ll.PrintL()
+
+}
---
+++ coding/ll/main_test.go
@@ -0,0 +1,85 @@
+package ll
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetNode(t *testing.T) {
+	cases := []struct {
+		pos   int
+		value int
+		want  int
+	}{
+		{pos: 0, value: 1, want: 1},
+		{pos: 1, value: 2, want: 2},
+		{pos: 2, value: 3, want: 3},
+		{pos: 3, value: 4, want: 4},
+	}
+
+	l := LinkedList{}
+
+	for _, c := range cases {
+		l.Insert(c.value)
+		node := l.GetNode(c.pos)
+		if node.value != c.want {
+			t.Errorf("GetNode error, value : %d should be %d", node.value, c.want)
+		}
+	}
+}
+
+func TestInsert(t *testing.T) {
+	cases := []struct {
+		value int
+		want  int
+	}{
+		{value: 1, want: 1},
+		{value: 2, want: 2},
+		{value: 3, want: 3},
+		{value: 4, want: 4},
+	}
+
+	l := LinkedList{}
+	for i, c := range cases {
+		l.Insert(c.value)
+		if l.len != i+1 {
+			t.Errorf("Insert error, length : %d should be %d", l.len, i+1)
+		}
+
+		if l.GetNode(i).value != c.want {
+			t.Errorf("Insert error, value : %d should be %d", l.head.value, c.want)
+		}
+	}
+}
+
+func TestInsertAt(t *testing.T) {
+	cases := []struct {
+		pos   int
+		value int
+		want  int
+	}{
+		{pos: 0, value: 0, want: 0},
+		{pos: 1, value: 2, want: 2},
+		{pos: 2, value: 3, want: 3},
+		{pos: 3, value: 4, want: 4},
+		{pos: 7, value: 5, want: 5},
+	}
+
+	l := LinkedList{}
+
+	l.Insert(11)
+	l.Insert(22)
+	l.Insert(33)
+	l.Insert(44)
+
+	for _, c := range cases {
+		l.InsertAt(c.pos, c.value)
+		node := l.GetNode(c.pos)
+		if node.value != c.want {
+			t.Errorf("InsertAt error, value : %d should be %d", node.value, c.want)
+		}
+	}
+	//
+	l.PrintL()
+	fmt.Printf("%+v\n", l)
+}
