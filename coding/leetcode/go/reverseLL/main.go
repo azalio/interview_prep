34dac26888b3431f63baf336f6165c01056f6789
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2023-02-14 02:22:55 +0300

    reverseLL

--- coding/leetcode/go/reverseLL/main.go
+++ coding/leetcode/go/reverseLL/main.go
@@ -1,12 +1,42 @@
 package reverseLL
 
+import "fmt"
+
 // ListNode Definition for singly-linked list.
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
+func PrintL(head *ListNode) {
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
+}
+
 func reverseList(head *ListNode) *ListNode {
 
-	return nil
+	//PrintL(head)
+
+	current := head
+	var next *ListNode = nil
+	var prev *ListNode = nil
+
+	for current != nil {
+		fmt.Println("+++++++++")
+
+		fmt.Printf("current: %v\n", current)
+		next = current.Next
+		fmt.Printf("next: %v\n", next)
+		current.Next = prev
+		fmt.Printf("current.next: %v\n", current.Next)
+		prev = current
+		fmt.Printf("prev: %v\n", prev)
+		current = next
+		fmt.Printf("current: %v\n", current)
+	}
+
+	return prev
+
 }
--- coding/leetcode/go/reverseLL/main_test.go
+++ coding/leetcode/go/reverseLL/main_test.go
@@ -1,7 +1,6 @@
 package reverseLL
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -9,90 +8,38 @@ var test = []struct {
 	got  []int
 	want []int
 }{
+	//{
+	//	[]int{1, 2, 3, 4, 5},
+	//	[]int{5, 4, 3, 2, 1},
+	//},
+	//{
+	//	[]int{1, 2},
+	//	[]int{2, 1},
+	//},
 	{
-		[]int{1, 2, 3, 4, 5},
-		[]int{5, 4, 3, 2, 1},
+		[]int{1, 2, 3},
+		[]int{3, 2, 1},
 	},
-	{
-		[]int{1, 2},
-		[]int{2, 1},
-	},
-	{
-		[]int{},
-		[]int{},
-	},
-}
-
-func (l *ListNode) Insert(val int) {
-	n := &ListNode{val, nil}
-	if l == nil {
-		l.Val = val
-		l.Next = nil
-	} else {
-		l.Next = n
-	}
-}
-
-func compareLL(one *ListNode, two *ListNode) bool {
-
-	//fmt.Printf("one: %v, two: %v\n", one, two)
-
-	for one.Next != nil && two.Next != nil {
-		if one.Val != two.Val {
-			return false
-		}
-
-		one = one.Next
-		two = two.Next
-	}
-	return true
-
-}
-
-func returnSliceFromLL(ll *ListNode) []int {
-	var sliceLL []int
-
-	if ll == nil {
-		return sliceLL
-	}
-
-	for ll.Next != nil {
-		sliceLL = append(sliceLL, ll.Val)
-		ll = ll.Next
-	}
-	return sliceLL
-}
-
-func PrintL(head *ListNode) {
-	for head.Next != nil {
-		fmt.Println(head.Val)
-		head = head.Next
-	}
+	//{
+	//	[]int{},
+	//	[]int{},
+	//},
 }
 
 func TestReverseList(t *testing.T) {
 
-	ll := &ListNode{}
-	reversedLL := &ListNode{}
+	//e := &ListNode{5, nil}
+	//d := &ListNode{4, e}
+	c := &ListNode{3, nil}
+	b := &ListNode{2, c}
+	a := &ListNode{1, b}
+	//
+	//PrintL(a)
+	//fmt.Println("==================")
+	reverseLL := reverseList(a)
 
-	for _, n := range test {
-		for _, v := range n.got {
-			ll.Insert(v)
-		}
+	t.Errorf("%v\n", reverseLL)
 
-		PrintL(ll)
+	//PrintL(reverseLL)
 
-		head := reverseList(ll)
-
-		for _, v := range n.want {
-			reversedLL.Insert(v)
-		}
-
-		if !compareLL(ll, reversedLL) {
-			t.Errorf("TestReverseList: Want %v, got: %v\n",
-				n.want, returnSliceFromLL(head))
-		}
-		ll = &ListNode{}
-		reversedLL = &ListNode{}
-	}
 }
