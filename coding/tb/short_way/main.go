06dfd58425dbfa1ab31c8522d5a701cfe478eabc
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2023-02-16 00:56:37 +0300

    AUaR ch1

--- coding/tb/short_way/main.go
+++ coding/tb/short_way/main.go
@@ -1,1 +1,34 @@
 package short_way
+
+import (
+	"fmt"
+	"strings"
+)
+
+//дан корректный путь для файла или директории,
+//содержащий в том числе конструкции вида ./ ../,
+//нужно упростить путь, насколько возможно
+//Пример "/a/b/c/./../../d" -> "/a/d"
+
+func shortWay(s string) string {
+
+	var sResult []string
+
+	ss := strings.Split(s, "/")
+
+	for _, c := range ss {
+		if c == "." {
+			continue
+		}
+		if c == ".." {
+			sResult = sResult[:len(sResult)-1]
+			fmt.Println(sResult)
+			continue
+		}
+		sResult = append(sResult, c)
+	}
+
+	result := strings.Join(sResult, "/")
+	return result
+
+}
--- coding/tb/short_way/main_test.go
+++ coding/tb/short_way/main_test.go
@@ -1,1 +1,26 @@
 package short_way
+
+import "testing"
+
+var test = []struct {
+	s    string
+	want string
+}{
+	{
+		"/a/b/c/./../../d",
+		"/a/d",
+	},
+	{
+		"/a/b/c",
+		"/a/b/c",
+	},
+}
+
+func TestShortWay(t *testing.T) {
+	for _, tt := range test {
+		result := shortWay(tt.s)
+		if result != tt.want {
+			t.Errorf("\nWant: %s, got: %s", tt.want, result)
+		}
+	}
+}
