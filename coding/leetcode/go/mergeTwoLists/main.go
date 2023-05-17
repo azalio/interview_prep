33ff5cd5ebb15f29e2c8bba8518984d3d15b4239
Mikhail Yu Petrov <mikhail.petrov@corp.mail.ru>
2023-05-18 01:29:05 +0300

    MergeLL

--- coding/leetcode/go/mergeTwoLists/main.go
+++ coding/leetcode/go/mergeTwoLists/main.go
@@ -1,1 +1,32 @@
 package mergeTwoLists
+
+/*
+*
+  - Definition for singly-linked list.
+  - type ListNode struct {
+  - Val int
+  - Next *ListNode
+  - }
+
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+
+	ll := ListNode{}
+
+	var p1, p2 int
+
+	for list1.Next != nil {
+		if list1.Val
+	}
+
+	return &ll
+}
--- coding/leetcode/go/mergeTwoLists/main_test.go
+++ coding/leetcode/go/mergeTwoLists/main_test.go
@@ -1,1 +1,75 @@
 package mergeTwoLists
+
+import (
+	"fmt"
+	"testing"
+)
+
+var test = []struct {
+	list1 []int
+	list2 []int
+	want  []int
+}{
+	{
+		[]int{1, 2, 4},
+		[]int{1, 3, 4},
+		[]int{1, 1, 2, 3, 4, 4},
+	},
+	//{
+	//	[]int{},
+	//	[]int{},
+	//	[]int{},
+	//},
+	//{
+	//	[]int{},
+	//	[]int{0},
+	//	[]int{0},
+	//},
+}
+
+func createLL(s []int) ListNode {
+	if len(s) == 0 {
+		return ListNode{}
+	}
+
+	if len(s) == 1 {
+		return ListNode{s[0], nil}
+	}
+
+	ll := ListNode{s[len(s)-1], nil}
+	//fmt.Printf("%v\n", ll)
+
+	for i := len(s) - 2; i >= 0; i-- {
+		oldLL := ll
+		ll = ListNode{s[i], &oldLL}
+		//fmt.Printf("%v\n", ll)
+
+	}
+
+	return ll
+}
+
+func printLL(ll ListNode) {
+	for {
+		fmt.Println(ll.Val)
+		if ll.Next == nil {
+			return
+		}
+		ll = *ll.Next
+	}
+}
+
+func TestMergeTwoLists(t *testing.T) {
+
+	for _, tt := range test {
+		ll1 := createLL(tt.list1)
+		ll2 := createLL(tt.list2)
+
+		//fmt.Println(ll1.Next.Next.Val)
+		//printLL(ll1)
+		//printLL(ll2)
+
+		result := mergeTwoLists(&ll1, &ll2)
+		printLL(*result)
+	}
+}
